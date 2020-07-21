b8da4355c1b1d30b5ccadc370dda03a21ceabe24
nguyen dang duy <nguyendangduynonglam@gmail.com>
2020-07-22 01:39:00 +0700

    Covert page html to cshtml file

---
+++ Website_StorePhone/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_StorePhone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult SingleBlog()
+        {
+            return View();
+        }
+        public ActionResult SingleProduct()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Website_StorePhone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Website_StorePhone.MvcApplication" Language="C#" %>
